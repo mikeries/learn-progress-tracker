2b1489c73ae2716177e56b28112562d6ca24b2eb
Michael Ries <michael.ries@gmail.com>
2017-05-15 13:13:59 -0500

    create CurriculumElement model and table

---
+++ app/models/curriculum_element.rb
@@ -0,0 +1,2 @@
+class CurriculumElement < ApplicationRecord
+end
---
+++ db/migrate/20170515180029_create_curriculum_elements.rb
@@ -0,0 +1,18 @@
+class CreateCurriculumElements < ActiveRecord::Migration[5.1]
+  def change
+    create_table :curriculum_elements do |t|
+      t.boolean :complete
+      t.datetime :started_at
+      t.boolean :viewed
+      t.string :content_type
+      t.string :slug
+      t.string :title
+      t.boolean :visible
+      t.boolean :completed_all
+      t.boolean :started_any
+      t.string :type
+
+      t.timestamps
+    end
+  end
+end
