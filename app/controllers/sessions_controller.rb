3605efbee6e4a9a5b243f6bf51823aede705bb84
Michael Ries <michael.ries@gmail.com>
2017-05-15 16:41:52 -0500

    add dashboard page and route there if student_signed_in?

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,10 @@
 class SessionsController < ApplicationController
+  def welcome
+    return redirect_to dashboard_path if student_signed_in?
+  end
+
+  def dashboard
+    return redirect_to root_path unless student_signed_in?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :students, :controllers => { :omniauth_callbacks => "students/omniauth_callbacks" }
 
   root to: "sessions#welcome"
+
+  get '/dashboard' => 'sessions#dashboard'
 end
