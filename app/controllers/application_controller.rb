ab38c5a5d65aae03abcd9013ffa8907b4512634c
Michael Ries <michael.ries@gmail.com>
2017-05-18 13:17:29 -0500

    minor view updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
+  def current_track
+    @current_track ||= current_student.current_track
+  end
 end
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,11 +2,11 @@ class LessonsController < ApplicationController
   before_action :authenticate_student!
 
   def index
-    @topics = current_student.topics.all
+    @topics = current_track.topics.all
   end
 
   def show
-    @lesson = current_student.find(params[:id])
+    @lesson = current_track.lessons.find(params[:id])
     @note = @lesson.notes.find_by(student_id: current_student.id)
   end
 end
