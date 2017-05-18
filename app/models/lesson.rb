aeaafbbd09018cc4507e9ba915ae5a186c5fffd3
Michael Ries <michael.ries@gmail.com>
2017-05-18 15:46:00 -0500

    restyle lesson#show with tags

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,7 +2,7 @@ class Lesson < CurriculumElement
   belongs_to :unit
   has_many :notes
   has_many :lesson_tags
-  has_many :tags, through: :lessons
+  has_many :tags, through: :lesson_tags
 
   LEARN_ROOT = 'https://learn.co/tracks/full-stack-web-dev-with-react/'
 
