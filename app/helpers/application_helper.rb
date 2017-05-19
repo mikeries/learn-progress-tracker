04e8d0261450cd4d060a4f9930d77dbb01a694aa
Michael Ries <michael.ries@gmail.com>
2017-05-19 09:15:15 -0500

    make breadcrumbs linkable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,3 @@
 module ApplicationHelper
 
-  def breadcrumb_menu(element)
-    links = ["Full Stack Web Dev. With React"]
-
-    case element.type
-    when 'Topic'
-      links << element.title
-    when 'Unit'
-      links << element.topic.title
-      links << element.title
-    when 'Lesson'
-      links << element.unit.topic.title
-      links << element.unit.title
-      links << element.title
-    end
-
-    links.join(" > ")
-  end
-
 end
