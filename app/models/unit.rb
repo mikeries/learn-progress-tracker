852f1d3c1a9edc4c3ce15a9bfd77235923412fbd
Michael Ries <michael.ries@gmail.com>
2017-05-30 22:33:48 -0500

    refactor unit.rb

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -3,15 +3,15 @@ class Unit < CurriculumElement
   has_many :lessons
 
   def lessons=(lessons_hash)
-    lessons_hash.each { |lesson_hash| self.lessons.build(lesson_hash) }
+    lessons_hash.each { |lesson_hash| lessons.build(lesson_hash) }
   end
 
   def student_id=(student_id)
     super(student_id)
-    lessons.each {|lesson| lesson.student_id=student_id}
+    lessons.each { |lesson| lesson.student_id = student_id }
   end
 
   def completed_lessons
-    lessons.select {|lesson| lesson.complete}.count
+    lessons.select(&complete).count
   end
 end
