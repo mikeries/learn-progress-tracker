0b1f403eb5fc8f419c34b299479deb690356bc81
Michael Ries <michael.ries@gmail.com>
2017-06-02 16:06:04 -0500

    wip on lesson-template.  Add url to lesson AMS

--- app/serializers/lesson_serializer.rb
+++ app/serializers/lesson_serializer.rb
@@ -1,5 +1,5 @@
 class LessonSerializer < ActiveModel::Serializer
-  attributes :id, :complete, :content_type, :slug, :title
+  attributes :id, :complete, :content_type, :slug, :title, :url
   has_many :notes
   has_many :tags
 end
