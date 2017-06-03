8371c213d8db1ede15c605c0c4a8ccd082bccfef
Michael Ries <michael.ries@gmail.com>
2017-06-03 16:47:20 -0500

    refactor lesson.js

--- app/assets/javascripts/learn-companion.js
+++ app/assets/javascripts/learn-companion.js
@@ -2,30 +2,6 @@
 
 $(function() {
     console.log('DOM is ready to go!');
-    $('#next-button').parent().on('click', function(e) {
-        e.preventDefault();
-
-        var $link = $(this);
-        var url = $link.attr('href');
-
-        // load next page
-        $.ajax({
-                url: url,
-                dataType: 'json',
-                method: 'GET'
-            })
-            .success(function(data) {
-                lesson = new Lesson(data);
-                var source = $('#lesson-template').html();
-                var template = Handlebars.compile(source);
-                var html = template(lesson);
-                $('#lesson-content').html(html);
-            })
-            .error(function(response) {
-                errorMessage("Oops" + response);
-            });
-
-    })
 })
 
 function errorMessage(message) {
--- app/assets/javascripts/lesson.js
+++ app/assets/javascripts/lesson.js
@@ -1,8 +1,47 @@
 function Lesson(attributes) {
     for (var key in attributes) {
-        this[key] = attributes[key]
+        this[key] = attributes[key];
     }
 }
 
-Lesson.prototype = Object.create(Unit.prototype);
-Lesson.prototype.constructor = Unit;
+$(function() {
+    Lesson.templateSrc = $('#lesson-template').html();
+    Lesson.template = Handlebars.compile(Lesson.templateSrc);
+
+    Lesson.addListeners();
+})
+
+Lesson.prototype.viewHtml = function() {
+    return Lesson.template(this);
+}
+
+Lesson.addListeners = function() {
+    Lesson.nextButtonListener()
+}
+
+Lesson.displayLesson = function(data) {
+    var lesson = new Lesson(data);
+    var html = lesson.viewHtml();
+    $('#lesson-content').html(html);
+    Lesson.addListeners();
+}
+
+Lesson.nextButtonListener = function() {
+    $('#next-button').parent().on('click', function(e) {
+        e.preventDefault();
+
+        var $link = $(this);
+        var url = $link.attr('href');
+
+        $.ajax({
+                url: url,
+                dataType: 'json',
+                method: 'GET'
+            })
+            .success(Lesson.displayLesson)
+            .error(function(response) {
+                errorMessage("Oops" + response);
+            });
+
+    })
+}
