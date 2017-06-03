445cc3a0fb4f552addd3c2b9a706fc1f2cb32023
Michael Ries <michael.ries@gmail.com>
2017-06-02 19:22:15 -0500

    retrieve next lesson data

--- app/assets/javascripts/learn-companion.js
+++ app/assets/javascripts/learn-companion.js
@@ -7,8 +7,22 @@ $(function() {
     console.log('DOM is ready to go!');
     $('#next-button').parent().on('click', function(e) {
         e.preventDefault();
+
+        var $link = $(this);
+        var url = $link.attr('href');
+
         // load next page
-        console.log('next');
+        $.ajax({
+                url: url,
+                dataType: 'json',
+                method: 'GET'
+            })
+            .success(function(data) {
+                console.log(data);
+            })
+            .error(function(response) {
+                errorMessage("Oops" + response);
+            })
     })
 })
 
