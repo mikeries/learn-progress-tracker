f4ca3cf50f19e7e4e610751617f7896443f3ee8f
Michael Ries <michael.ries@gmail.com>
2017-07-11 14:27:39 -0500

    remove commented out code

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'json'
-
-# file = File.read(Rails.root + 'db/curriculum-data.json')
-# track_hash = JSON.parse(file)
-#
-# initializer = Student.create(email: 'initialize@student.edu', password: 'initialize')
-# initializer.tracks.create(track_hash)
