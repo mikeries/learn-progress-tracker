73e933039e343b6a6ddccda0a8b78eba2300dead
Michael Ries <michael.ries@gmail.com>
2017-07-11 12:17:13 -0500

    split functions related to rendering the unit index into unitIndex.js

--- app/assets/javascripts/lesson.js
+++ app/assets/javascripts/lesson.js
@@ -142,50 +142,9 @@ Lesson.getLesson = function(url) {
     });
 }
 
-Lesson.prototype.indexTitleHtml = function() {
-    return `
-    <div>
-        <i class="fa ${this.statusIconName}"></i>
-        <i class="fa ${this.typeIconName}"></i>
-        <a href="/lessons/${this.id}">${this.title}</a>
-    </div>`
-}
-
-Lesson.unitHtml = function(lessons) {
-    let html = '';
-    for (let index in lessons) {
-        const lesson = new Lesson(lessons[index]);
-        html += lesson.indexTitleHtml()
-    }
-    return html
-}
-
-Lesson.getUnitIndex = function(ev) {
-    unitId = $(ev.currentTarget).data().unitId;
-    $unitIndexContainer = $(`#unit-${unitId}`)
-
-    if ($unitIndexContainer.children().length === 0) {
-        $.ajax({
-            url: `/lessons/units/${unitId}`,
-            dataType: 'json',
-            method: 'GET'
-        })
-        .success((data) => {
-            const html = Lesson.unitHtml(data);
-            $unitIndexContainer.empty().append(html);
-        })
-        .fail(() => {
-            errorMessage(`Oops! Failed to load '${url}'.`);
-        });
-    }
-}
-
 $(function() {
     if ($('body').hasClass("lessons show")) {
         Lesson.initializeHandlebars();
         Lesson.getLesson($('#lesson-content').data().lessonUrl);
-    }
-    if ($('body').hasClass("lessons index")) {
-        $('.unit a').on('click', Lesson.getUnitIndex)
     }
 })
